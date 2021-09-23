e3bf8d085f1defde621f7d4be6b2a822456a7518
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-09-23 15:56:29 -0700

    netty epoll + kqueue

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.kqueue.KQueue;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("Epoll (Linux): " + Epoll.isAvailable());
+		System.out.println("KQueue (OSX): " + KQueue.isAvailable());
 	}
 }
